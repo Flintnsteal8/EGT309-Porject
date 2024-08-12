ee155a3c3032395333f37db9a530c6df4d4b4935
Aloysius <seealoysius1@gmail.com>
2024-08-12 17:07:10 +0800

    fixed the prediction part of the comparison file

--- ljx project/comparison.py
+++ ljx project/comparison.py
@@ -1,5 +1,7 @@
 import tensorflow as tf
 import matplotlib.pyplot as plt
+import PIL.Image as Image
+import numpy as np
 from sklearn.metrics import confusion_matrix, classification_report
 # Import the test generator function from data_augmentation.py
 from data_augmentation import create_test_generator
@@ -13,13 +15,43 @@ transfer_model = tf.keras.models.load_model('transfer_learning_model.h5')
 test_dir = 'dataset_transport/dataset_transport/test'
 test_generator = create_test_generator(test_dir)
 
-# # Predict using CNN model
-cnn_predictions = cnn_model.predict(test_generator)
-cnn_pred_labels = cnn_predictions.argmax(axis=1)
+# Define class labels
+class_labels = ['airplane', 'automobile', 'ship', 'truck']
 
-# Predict using transfer learning model
-transfer_predictions = transfer_model.predict(test_generator)
-transfer_pred_labels = transfer_predictions.argmax(axis=1)
+# Define image paths
+cnn_test_image_path = 'dataset_transport/dataset_transport/test/automobile/0499.png'
+transfer_test_image_path = 'dataset_transport/dataset_transport/test/automobile/0499.png'  # Use a different image if needed
+
+# Load and preprocess images for CNN model
+cnn_img = Image.open(cnn_test_image_path).resize((32, 32))
+cnn_img_array = np.array(cnn_img) / 255.0
+cnn_img_array = cnn_img_array[np.newaxis, ...]  # Add batch dimension
+
+# Make prediction using CNN model
+cnn_result = cnn_model.predict(cnn_img_array)
+cnn_predicted_class = np.argmax(cnn_result[0], axis=-1)
+
+# Load and preprocess images for Transfer Learning model
+transfer_img = Image.open(transfer_test_image_path).resize((32, 32))
+transfer_img_array = np.array(transfer_img) / 255.0
+transfer_img_array = transfer_img_array[np.newaxis, ...]  # Add batch dimension
+
+# Make prediction using Transfer Learning model
+transfer_result = transfer_model.predict(transfer_img_array)
+transfer_predicted_class = np.argmax(transfer_result[0], axis=-1)
+
+# Plotting
+plt.figure(figsize=(20, 8))
+
+plt.subplot(1, 2, 1)
+plt.imshow(cnn_img)
+plt.title(f"CNN Model Prediction: {class_labels[cnn_predicted_class]}")
+
+plt.subplot(1, 2, 2)
+plt.imshow(transfer_img)
+plt.title(f"Transfer Learning Model Prediction: {class_labels[transfer_predicted_class]}")
+
+plt.show()
 
 # Get true labels from the test generator
 true_labels = test_generator.classes

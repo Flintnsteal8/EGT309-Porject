33957a626e82d290c14443600e2dfa8153fb82cb
Aloysius <seealoysius1@gmail.com>
2024-08-12 22:29:24 +0800

    Made it so that the model saves into a saved_models file so that it can be pulled from there later on

--- ljx project/comparison.py
+++ ljx project/comparison.py
@@ -1,8 +1,9 @@
-mport tensorflow as tf
+import tensorflow as tf
 import matplotlib.pyplot as plt
 import PIL.Image as Image
 import numpy as np
-from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix, classification_report
+# Import the test generator function from data_augmentation.py
 from data_augmentation import create_test_generator
 
 
@@ -18,37 +19,27 @@ test_generator = create_test_generator(test_dir)
 class_labels = ['airplane', 'automobile', 'ship', 'truck']
 
 # Define image paths
-image_paths = [
-    'dataset_transport/dataset_transport/test/automobile/0499.png',
-    'dataset_transport/dataset_transport/test/airplane/0409.png',
-    'dataset_transport/dataset_transport/test/truck/0259.png',
-    'dataset_transport/dataset_transport/test/ship/0100.png'
-]
+cnn_test_image_path = 'dataset_transport/dataset_transport/test/airplane/0499.png'
+transfer_test_image_path = 'dataset_transport/dataset_transport/test/airplane/0499.png'  # Use a different image if needed
 
-# Initialize lists to hold results for plotting
-cnn_predictions = []
-transfer_predictions = []
-images = []
+# Load and preprocess images for CNN model
+cnn_img = Image.open(cnn_test_image_path).resize((32, 32))
+cnn_img_array = np.array(cnn_img) / 255.0
+cnn_img_array = cnn_img_array[np.newaxis, ...]  # Add batch dimension
 
-# Process each image
-for image_path in image_paths:
-    # Load and preprocess image for CNN model
-    img = Image.open(image_path).resize((32, 32))
-    img_array = np.array(img) / 255.0
-    img_array = img_array[np.newaxis, ...]  # Add batch dimension
+# Make prediction using CNN model
+cnn_result = cnn_model.predict(cnn_img_array)
+cnn_predicted_class = np.argmax(cnn_result[0], axis=-1)
 
-    # Make prediction using CNN model
-    cnn_result = cnn_model.predict(img_array)
-    cnn_predicted_class = np.argmax(cnn_result[0], axis=-1)
-    cnn_predictions.append(cnn_predicted_class)
+# Load and preprocess images for Transfer Learning model
+transfer_img = Image.open(transfer_test_image_path).resize((32, 32))
+transfer_img_array = np.array(transfer_img) / 255.0
+transfer_img_array = transfer_img_array[np.newaxis, ...]  # Add batch dimension
 
-    # Make prediction using Transfer Learning model
-    transfer_result = transfer_model.predict(img_array)
-    transfer_predicted_class = np.argmax(transfer_result[0], axis=-1)
-    transfer_predictions.append(transfer_predicted_class)
+# Make prediction using Transfer Learning model
+transfer_result = transfer_model.predict(transfer_img_array)
+transfer_predicted_class = np.argmax(transfer_result[0], axis=-1)
 
-    # Save the image for plotting
-    images.append(img)
 
 # # Predict using CNN model for classification report
 cnn_predictions = cnn_model.predict(test_generator)
@@ -57,25 +48,22 @@ cnn_pred_labels = cnn_predictions.argmax(axis=1)
 # Predict using transfer learning model for classification report
 transfer_predictions = transfer_model.predict(test_generator)
 transfer_pred_labels = transfer_predictions.argmax(axis=1)
-
-# Plotting the images with predictions
+# Plotting
 plt.figure(figsize=(20, 8))
 
-for i in range(4):
-    plt.subplot(2, 4, i + 1)
-    plt.imshow(images[i])
-    plt.title(f"CNN Prediction: {class_labels[cnn_predictions[i]]}")
-    plt.axis('off')
+plt.subplot(1, 2, 1)
+plt.imshow(cnn_img)
+plt.title(f"CNN Model Prediction: {class_labels[cnn_predicted_class]}")
 
-    plt.subplot(2, 4, i + 5)
-    plt.imshow(images[i])
-    plt.title(f"Transfer Prediction: {class_labels[transfer_predictions[i]]}")
-    plt.axis('off')
+plt.subplot(1, 2, 2)
+plt.imshow(transfer_img)
+plt.title(f"Transfer Learning Model Prediction: {class_labels[transfer_predicted_class]}")
 
 plt.show()
 
 # Get true labels from the test generator
 true_labels = test_generator.classes
+
 
 # Classification reports
 cnn_cr = classification_report(true_labels, cnn_pred_labels, target_names=test_generator.class_indices.keys())
